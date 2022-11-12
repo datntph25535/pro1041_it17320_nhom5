96c1fc9e8cd136c87715b1d8e4d597b08c0c5f60
datntph25535 <datntph25535@fpt.edu.vn>
2022-11-12 08:48:50 +0700

    Khoi tao project

---
+++ DemoProject/src/demoproject/DemoProject.java
@@ -0,0 +1,22 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package demoproject;
+
+/**
+ *
+ * @author PC
+ */
+public class DemoProject {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        System.out.println("12/11/2022");
+    }
+    
+}
